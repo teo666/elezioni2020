20040c2c746c7422e03a6cdef08c864f0d2983aa
teo <basili.teo@gmail.com>
2020-09-20 15:14:24 +0200

    referendum

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 const fs = require('fs');
 const express = require('express');
+const { commands } = require("npm");
 
 // Constants
 const PORT = 7777;
@@ -8,43 +9,51 @@ const HOST = '0.0.0.0';
 
 let results = []
 
-const commands = [
+const elezioni_commands = [
     "curl 'https://eleapi.interno.gov.it/siel/PX/scrutiniG/DE/20200920/TE/08/PR/043/CM/0330' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://elezioni.interno.gov.it/comunali/scrutini/20200920/scrutiniGI090430330' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' -H 'Content-Type: application/json' --compressed",
     "curl 'https://eleapi.interno.gov.it/siel/PX/scrutiniG/DE/20200920/TE/08/PR/043/CM/0291' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://elezioni.interno.gov.it/comunali/scrutini/20200920/scrutiniGI090430291' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' -H 'Content-Type: application/json' --compressed",
     "curl 'https://eleapi.interno.gov.it/siel/PX/scrutiniG/DE/20200920/TE/08/PR/043/CM/0110' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://elezioni.interno.gov.it/comunali/scrutini/20200920/scrutiniGI090430110' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' -H 'Content-Type: application/json' --compressed"
 ]
 
+const referendum_commands = [
+    "curl 'https://eleapi.interno.gov.it/siel/PX/scrutiniFI/DE/20200920/TE/09/SK/01/RE/09/PR/043' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://elezioni.interno.gov.it/referendum/scrutini/20200920/scrutiniFI01090430000' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' -H 'Content-Type: application/json' --compressed", // provincia
+    "curl 'https://eleapi.interno.gov.it/siel/PX/scrutiniFI/DE/20200920/TE/09/SK/01/RE/09' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://elezioni.interno.gov.it/referendum/scrutini/20200920/scrutiniFI01090000000' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' -H 'Content-Type: application/json' --compressed", //regione
+    "curl 'https://eleapi.interno.gov.it/siel/PX/scrutiniFI/DE/20200920/TE/09/SK/01' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://elezioni.interno.gov.it/referendum/scrutini/20200920/scrutiniFI01' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' -H 'Content-Type: application/json' --compressed" // nazionali
+]
+
 
 function execCommand(cmd) {
     return new Promise((resolve, reject) => {
-        console.log('fetching')
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 reject(error)
             } else {
-                resolve(JSON.parse(JSON.stringify(JSON.parse(stdout).cand)))
+                resolve(JSON.parse(stdout))
             }
         })
     })
 }
 
-function fetchData() {
-
-    return commands.map((c) => {
+function fetchData(arr) {
+    return arr.map((c) => {
         return execCommand(c)
     })
 }
 
-Promise.all(fetchData()).then((result) => {
-    const reducer = (accumulator, currentValue) => [...accumulator, ...currentValue];
-    results = result.reduce(reducer, []);
-})
-
 const app = express();
 
 app.get('/', (req, res) => {
-    Promise.all(fetchData()).then((result) => {
-        const reducer = (accumulator, currentValue) => [...accumulator, ...currentValue];
+    Promise.all(fetchData(elezioni_commands)).then((result) => {
+        const reducer = (accumulator, currentValue) => [...accumulator, ...currentValue.cand];
+        results = result.reduce(reducer, []);
+
+        res.send(results)
+    })
+});
+
+app.get('/referendum', (req, res) => {
+    Promise.all(fetchData(referendum_commands)).then((result) => {
+        const reducer = (accumulator, currentValue) => [...accumulator, ...currentValue.scheda];
         results = result.reduce(reducer, []);
 
         res.send(results)
